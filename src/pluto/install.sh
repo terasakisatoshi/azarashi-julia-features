b9e4c3c7fabf2cf10948badf98c05b1f8bf6a614
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-08-10 13:26:44 +0900

    Nannmo wakaran

--- src/pluto/install.sh
+++ src/pluto/install.sh
@@ -2,6 +2,12 @@
 set -e
 
 VERSION=${VERSION:-"0.19"}
-${HOME}/.juliaup/bin/julia -e "using Pkg; Pkg.add(name=\"Pluto\", version=\"$VERSION\")"
+USERNAME=${USERNAME:-${_REMOTE_USER:-"automatic"}}
+
+if [ "${USERNAME}" = "root" ]; then
+    /root/.juliaup/bin/julia -e "using Pkg; Pkg.add(name=\"Pluto\", version=\"$VERSION\")"
+else
+    /home/${USERNAME}/.juliaup/bin/julia -e "using Pkg; Pkg.add(name=\"Pluto\", version=\"$VERSION\")"
+fi
 
 echo "Done!"
