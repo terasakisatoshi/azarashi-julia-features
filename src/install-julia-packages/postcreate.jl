82c83da2255722e50a1edbcc50b1fa3121bc3fdb
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-08-10 23:10:07 +0900

    Kondo koso

--- src/install-julia-packages/postcreate.jl
+++ src/install-julia-packages/postcreate.jl
@@ -4,8 +4,8 @@ using Pkg
 Pkg.activate() # activate the default environment
 
 for p in split(read(joinpath(@__DIR__, "JULIAPACKAGES"), String))
-    if p in map(info -> info.name, values(Pkg.dependencies()))
-        println("Installing package $p")
+    if !(p in map(info -> info.name, values(Pkg.dependencies())))
+        @info "Installing package $p"
         Pkg.add(p)
     end
 end
