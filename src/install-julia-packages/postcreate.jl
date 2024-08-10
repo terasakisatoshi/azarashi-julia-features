d0fe1832b3b1cdf1c29a899268285af760089452
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-08-10 23:01:49 +0900

    Fix

--- src/install-julia-packages/postcreate.jl
+++ src/install-julia-packages/postcreate.jl
@@ -3,11 +3,9 @@
 using Pkg
 Pkg.activate() # activate the default environment
 
-if isfile(joinpath(@__DIR__, "JULIAPACKAGES"))
-    for p in split(read(joinpath(@__DIR__, "JULIAPACKAGES"), String))
-        if p in map(info -> info.name, values(Pkg.dependencies()))
-            println("Installing package $p")
-            Pkg.add(p)
-        end
+for p in split(read(joinpath(@__DIR__, "JULIAPACKAGES"), String))
+    if p in map(info -> info.name, values(Pkg.dependencies()))
+        println("Installing package $p")
+        Pkg.add(p)
     end
 end
