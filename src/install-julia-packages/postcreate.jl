aecb8b1431895a6d7e6109b88d500776eae0766c
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-08-10 19:07:42 +0900

    Update postcreate.jl

--- src/install-julia-packages/postcreate.jl
+++ src/install-julia-packages/postcreate.jl
@@ -5,7 +5,9 @@ Pkg.activate() # activate the default environment
 
 if isfile(joinpath(@__DIR__, "JULIAPACKAGES"))
     for p in split(read(joinpath(@__DIR__, "JULIAPACKAGES"), String))
-        println("Installing package $p")
-        Pkg.add(p)
+        if p in map(info -> info.name, values(Pkg.dependencies()))
+            println("Installing package $p")
+            Pkg.add(p)
+        end
     end
 end
